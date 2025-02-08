4c54dab529e9d4c52db903a59b942fdac5f4a8dc
Tarun Kumar <tarunvelagala80@gmail.com>
2025-02-08 11:57:42 +0000

    Deploy Vite App to GitHub pages https://tinyurl.com/3yncdeyn

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/v1/',
   server: {
     allowedHosts: [
       '5173-tarunvelagala-v1-2md0ne6eh7r.ws-us117.gitpod.io'
